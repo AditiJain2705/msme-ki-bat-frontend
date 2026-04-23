804602d09251f3cc23e9d1143c47264c210aa2ae
AditiJain2705 <jaditi921@gmail.com>
2026-04-23 19:40:16 +0530

    sitemap fixed

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/layout/Footer";
 import TopAppBar from "@/components/new-home/Topappbar";
 import BottomNavBar from "@/components/new-home/BottomNavBar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export default function RootLayout({ children }) {
   return (
