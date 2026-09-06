5546430e24d9ab8f3fb20730a6c0a6d374c63693
AditiJain2705 <jaditi921@gmail.com>
2026-09-06 22:29:20 +0530

    video sequence changed

--- components/new-home/videos.js
+++ components/new-home/videos.js
@@ -1,22 +1,6 @@
 export const VIDEOS = [
   {
     id: 1,
-    youtubeId: "x_fsg3BeR5o",
-    duration: "1:04",
-    title:
-      " आज पूरी दुनिया एक चीज़ ढूंढ रही है Trusted aur Resilient Manufacturers.",
-    description: "",
-  },
-  {
-    id: 2,
-    youtubeId: "aQ_9aVEkdys",
-    duration: "0:58",
-    title:
-      "भारत सरकार ने MSMEs के लिए पूरे देश में 100+ Technology Centres का network बनाया है, जिन पर हजारों करोड़ रुपये का investment किया गया है।",
-    description: "",
-  },
-  {
-    id: 3,
     youtubeId: "i3kovvRZgq0",
     duration: "",
     title:
@@ -26,12 +10,28 @@ export const VIDEOS = [
       "/documents/MSME_ki_baat_-_NSIC_RAW_MATERIAL_ASSISTANCE_SCHEME.pdf",
   },
   {
-    id: 4,
+    id: 2,
     youtubeId: "pD3EL68sP-Y",
     duration: "",
     title:
       "Export Badhane Ke Liye Sarkar Ka Bada Kadam:Piyush Goyal Ka 7-Point Agenda",
     description: "",
     documentLink: "/documents/MSME_ki_baat_-_7_POINT_EXPORT_AGENDA.pdf",
+  },
+  {
+    id: 3,
+    youtubeId: "x_fsg3BeR5o",
+    duration: "1:04",
+    title:
+      " आज पूरी दुनिया एक चीज़ ढूंढ रही है Trusted aur Resilient Manufacturers.",
+    description: "",
+  },
+  {
+    id: 4,
+    youtubeId: "aQ_9aVEkdys",
+    duration: "0:58",
+    title:
+      "भारत सरकार ने MSMEs के लिए पूरे देश में 100+ Technology Centres का network बनाया है, जिन पर हजारों करोड़ रुपये का investment किया गया है।",
+    description: "",
   },
 ];
